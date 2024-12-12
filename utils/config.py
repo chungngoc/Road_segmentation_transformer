91851945953a7bd2fe840041b9d4218f3ef63dd7
chungngoc <tongngocchungbtqb@gmail.com>
2024-12-12 02:14:12 +0100

    some config

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+ALL_CLASSES = [
+    '_background_',
+    'road',
+    'line_solid',
+    'line_dashed'
+]
+
+LABEL_COLORS_LIST = [
+    [0,0,0], # Background
+    [128,0,0],
+    [0,128,0],
+    [128,128,0]
+]
+
+VIS_LABEL_MAP = [
+    [0,0,0], # Background
+    [128,0,0],
+    [0,128,0],
+    [128,128,0]
+]
+
+IMG_MEAN = [0.485, 0.456, 0.406]
+IMG_STD = [0.229, 0.224, 0.225]
