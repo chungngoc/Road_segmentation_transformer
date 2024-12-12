eb7925350eab62066a32850a786bf5616b6cea0c
chungngoc <tongngocchungbtqb@gmail.com>
2024-12-12 02:15:26 +0100

    define model

---
+++ model/model.py
@@ -0,0 +1,12 @@
+from transformers import SegformerForSemanticSegmentation
+
+def segformer_model(classes):
+    '''
+        Build the SegFormer model using the MiT-B1 encoder which has been pretrained on the ImageNet-1K dataset
+    '''
+    model = SegformerForSemanticSegmentation.from_pretrained(
+        'nvidia/mit-b3',
+        num_labels = len(classes)
+    )
+
+    return model
